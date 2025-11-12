eb91ed97ab5670d89f6d118f82e9b9556d61b677
AstridCMelo <est.astridc.melo@unimilitar.edu.cogit config --global user.email>
2025-11-12 15:17:25 -0500

    Cambios script iconos de ejmplo

--- Assets/Scripts/ChangeTitle.cs
+++ Assets/Scripts/ChangeTitle.cs
@@ -5,16 +5,23 @@ using TMPro;
 
 public class ChangeTitle : MonoBehaviour
 {
+    public GameObject iconosEstudio;
+    public GameObject iconosPicnic;
+
     // Start is called before the first frame update
     void Start()
     {
         if(ChosenScene.selected == "Escena estudio")
         {
             GetComponent<TMP_Text>().text = "Estudio acogedor";
+            iconosEstudio.SetActive(true);
+            iconosPicnic.SetActive(false);
         }
         else if(ChosenScene.selected == "Escena picnic")
         {
             GetComponent<TMP_Text>().text = "Picnic";
+            iconosEstudio.SetActive(false);
+            iconosPicnic.SetActive(true);
         }
 
        
